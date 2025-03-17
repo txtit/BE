690936761eae7d2f9cb7072657c81d2913dfb2df
Xuan Thao <thao16068@gmail.com>
2025-03-17 10:54:57 +0700

    add cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://fe-theta-five.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
